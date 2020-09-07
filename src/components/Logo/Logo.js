366a2c86d15b92c7e1d33693861b35b4302c2140
Caleb <c-lab@Calebs-MacBook-Pro-2.local>
2020-09-06 21:00:45 -0400

    adding nav logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -2,20 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import IconLogo from './IconLogo';
+import LogoImage from './DesktopLogo.png';
 import css from './Logo.css';
 
 const Logo = props => {
   const { className, format, ...rest } = props;
-  const mobileClasses = classNames(css.logoMobile, className);
-
-  // If you want to use image instead of svg as a logo you can use the following code.
-  // Also, remember to import the image as LogoImage here.
-  // <img className={className} src={LogoImage} alt={config.siteTitle} {...rest} />
+  const isMobile = format !== 'desktop';
+  const classes = classNames(className, { [css.logoMobile]: isMobile });
+  
 
   return (
-    <IconLogo
-      className={format === 'desktop' ? className : mobileClasses}
-      format={format}
+    <img
+      className={classes}
+      src={LogoImage}
+      alt="Jaeger Logo"
       {...rest}
     />
   );
