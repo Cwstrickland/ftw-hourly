71f51e09c3223f058f9f13535611c550e3922018
Caleb <c-lab@Calebs-MacBook-Pro-2.local>
2020-11-25 10:56:55 -0500

    updating content on business page

--- src/containers/Businesses/Businesses.js
+++ src/containers/Businesses/Businesses.js
@@ -10,6 +10,8 @@ import {
 
 import css from './Businesses.css';
 import image from './sales-meeting.jpg';
+import image2 from './business-2.jpg';
+import image3 from './guy-on-tablet.jpg';
 
 const Businesses = () => {
    return (
@@ -33,8 +35,29 @@ const Businesses = () => {
                </h1>
                <img className={css.coverImage} src={image} alt="sales meeting" />
                <div>
-                  <p>This is where all the words go. Lot's of them.</p>
-                  <p>We should explain to businesses why this is the place to find sales pros, and a breif overview of how it works.</p>
+                  <p>Do you have a sales opportunity in your small business, but your payroll cannot afford another full time employee? Are you always forced to settle for lesser sales options due to the size of your business? The Jaeger Co is the best solution for this problem via our marketplace of highly skilled and diverse pool professional salespeople that we call “Jaegers.” These Jaegers are on demand, freelance hires to help even the playing field for you against competitors in your industry. Armed with Jaegers on demand, you can now hunt more sales opportunities, and be more competitive in your market than ever before. Hire only when you need them, and expand your business as much or as little as payroll allows.
+                  </p>
+                  <p>Let’s break down on how it works:</p>
+                  <ol>
+                     <li>You have a sales opportunity</li>
+                     <li>Go online <a href="/s">HERE</a> to browse our Jaegers</li>
+                     <li>Work with your chose Jaeger to negotiate the project scope</li>
+                     <li>Sit back and enjoy while your new Jaeger goes on the hunt for you</li>
+                     <li>Profit</li>
+                  </ol>
+                  <img className={css.coverImage} src={image2} alt="sales meeting 2" />
+                  <p>That’s it! When you need sales on demand, you need a skilled sales hunter… You need a Jaeger.</p>
+                  <p>From our plan you gain:</p>
+                  <ul>
+                     <li>- Freedom and flexibility</li>
+                     <li>- Access talent</li>
+                     <li>- The inescapable frustration of sales staffing</li>
+                  </ul>
+                  <img className={css.coverImage} src={image3} alt="Guy on tablet" />
+                  <p>No longer will you get passed over simply because you are a small business. And the competition among Jaegers sharpens this free market on a constant basis. Every completed project receives a review from both the business, and the Jaeger. The cream will always rise to the top.</p>
+                  <p>Now you’re ready to take control of the hunt for success!</p>
+                  <a href="/signup">SIGN UP HERE TODAY.</a>
+
                </div>
 
             </LayoutWrapperMain>
